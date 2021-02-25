f07f029d1e2af795a5a2e3bfe0868a22f046d955
ukeskin <ukeskin2626@gmail.com>
2021-02-25 17:16:23 +0300

    split components ✂

--- src/App.js
+++ src/App.js
@@ -1,109 +1,24 @@
 import React, { useState } from "react";
+import Products from "./Products";
+import Cart from "./Cart";
 
 const PAGE_PRODUCTS = "products";
 const PAGE_CART = "cart";
 
 function App() {
-  const [cart, setCart] = useState([]);
   const [page, setPage] = useState([PAGE_PRODUCTS]);
-  const [products] = useState([
-    {
-      name: "Headphones v2",
-      cost: "$22.99",
-      image:
-        "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
-    },
-    {
-      name: "Headphones v1",
-      cost: "$12.99",
-      image:
-        "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
-    },
-    {
-      name: "Headphones v1",
-      cost: "$12.99",
-      image:
-        "https://images.unsplash.com/photo-1494726161322-5360d4d0eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=80",
-    },
-    {
-      name: "Headphones v1",
-      cost: "$12.99",
-      image:
-        "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
-    },
-  ]);
-
-  const addCart = (product) => {
-    setCart([...cart, product]);
+  const [cart, setCart] = useState([]);
+  const removeFromCart = (productToremove) => {
+    setCart(cart.filter((product) => product !== productToremove));
+  };
+  const addToCart = (product) => {
+    setCart([...cart, { ...product }]);
   };
 
   const navigateTo = (nextPage) => {
     setPage(nextPage);
   };
 
-  const renderProducts = () => (
-    <div className="grid md:grid-cols-4 grid-cols-1 gap-8 w-full h-16 p-4">
-      {products.map((product, idx) => (
-        <div className="py-6" key={idx}>
-          <div className="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
-            <div
-              className="w-1/3  "
-              style={{ backgroundImage: `url('${product.image}')` }}
-            ></div>
-            <div className="w-2/3 p-4">
-              <h1 className="text-gray-900 font-bold text-2xl">
-                {product.name}
-              </h1>
-              <div className="flex item-center mt-2"></div>
-              <div className="flex item-center justify-between mt-3">
-                <h1 className="text-gray-700 font-bold text-xl">
-                  {product.cost}
-                </h1>
-                <button
-                  onClick={() => addCart(product)}
-                  className="px-3 py-2 bg-red-400 hover:bg-red-500 text-white text-xs font-bold uppercase rounded"
-                >
-                  Add to Card
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-
-  const renderCart = () => (
-    <div className="grid md:grid-cols-4 grid-cols-1 gap-4 w-full h-16 p-4">
-      {cart.map((product, idx) => (
-        <div className="py-6" key={idx}>
-          <div className="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
-            <div
-              className="w-1/3  "
-              style={{ backgroundImage: `url('${product.image}')` }}
-            ></div>
-            <div className="w-2/3 p-4">
-              <h1 className="text-gray-900 font-bold text-2xl">
-                {product.name}
-              </h1>
-              <div className="flex item-center mt-2"></div>
-              <div className="flex item-center justify-between mt-3">
-                <h1 className="text-gray-700 font-bold text-xl">
-                  {product.cost}
-                </h1>
-                <button
-                  onClick={() => addCart(product)}
-                  className="px-3 py-2 bg-red-500 hover:bg-red-600 text-white text-xs font-bold uppercase rounded"
-                >
-                  Remove
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
   return (
     <div>
       <header className="flex justify-between bg-red-400 p-4 text-white">
@@ -143,8 +58,10 @@ function App() {
           </span>
         </button>
       </header>
-      {page == PAGE_PRODUCTS && renderProducts()}
-      {page == PAGE_CART && renderCart()}
+      {page == PAGE_PRODUCTS && <Products addToCart={addToCart} />}
+      {page == PAGE_CART && (
+        <Cart cart={cart} removeFromCart={removeFromCart} />
+      )}
     </div>
   );
 }
---
+++ src/Products.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+export default function Products({ addToCart }) {
+  const [products] = useState([
+    {
+      name: "Headphones v0",
+      cost: "$2.99",
+      image:
+        "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+    },
+    {
+      name: "Headphones v1",
+      cost: "$12.99",
+      image:
+        "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+    },
+    {
+      name: "Headphones v2",
+      cost: "$14.99",
+      image:
+        "https://images.unsplash.com/photo-1494726161322-5360d4d0eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=80",
+    },
+    {
+      name: "Headphones v3",
+      cost: "$16.99",
+      image:
+        "https://images.unsplash.com/photo-1547949003-9792a18a2601?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+    },
+  ]);
+
+  return (
+    <div className="grid md:grid-cols-4 grid-cols-1 gap-8 w-full h-16 p-4">
+      {products.map((product, idx) => (
+        <div className="py-6" key={idx}>
+          <div className="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
+            <div
+              className="w-1/3  "
+              style={{ backgroundImage: `url('${product.image}')` }}
+            ></div>
+            <div className="w-2/3 p-4">
+              <h1 className="text-gray-900 font-bold text-2xl">
+                {product.name}
+              </h1>
+              <div className="flex item-center mt-2"></div>
+              <div className="flex item-center justify-between mt-3">
+                <h1 className="text-gray-700 font-bold text-xl">
+                  {product.cost}
+                </h1>
+                <button
+                  onClick={() => addToCart(product)}
+                  className="px-3 py-2 bg-red-400 hover:bg-red-500 text-white text-xs font-bold uppercase rounded"
+                >
+                  Add to Card
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
